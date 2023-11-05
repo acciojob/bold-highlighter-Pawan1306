f882b8bec3e1d6068d0549bec08566701c5741d3
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-11-05 20:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function highlight() {
-    //Write your code here
+    // Get all <strong> elements in the paragraph
+    const strongElements = document.querySelectorAll('strong');
 
-
+    // Change the color of bold elements to green
+    strongElements.forEach((element) => {
+        element.style.color = 'green';
+    });
 }
-
 
 function return_normal() {
-    //Write your code here
+    // Get all <strong> elements in the paragraph
+    const strongElements = document.querySelectorAll('strong');
 
-    
+    // Revert the color of bold elements back to black
+    strongElements.forEach((element) => {
+        element.style.color = 'black';
+    });
 }
+
